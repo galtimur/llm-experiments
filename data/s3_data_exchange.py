6dcb4aa97f9aa41a64de2f44e914843d91e7b9b0
galtimur <gal_timur@yahoo.com>
2024-07-19 11:22:04 +0200

    Started organizing the repo

---
+++ data/s3_data_exchange.py
@@ -0,0 +1,136 @@
+import boto3
+import os
+from pathlib import Path
+from tqdm import tqdm
+
+
+def upload_file_s3(
+        checkpoint_file, s3_checkpoint_file, bucket_name="jettrain-experiments"
+):
+    # Initialize the S3 client
+    aws_access_key_id, aws_secret_access_key = (
+        os.environ["AWS_KEY"],
+        os.environ["AWS_SECRET"],
+    )
+    s3 = boto3.client(
+        "s3",
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key,
+    )
+
+    # Iterate over the files in the local directory
+    if os.path.isfile(checkpoint_file):
+        # Generate the S3 key for the file
+        s3_key = s3_checkpoint_file
+
+        # Full path to the local file
+        local_file_path = checkpoint_file
+
+        # Upload the file to S3
+        s3.upload_file(local_file_path, bucket_name, s3_key)
+        print(f"Uploaded {checkpoint_file} to S3://{bucket_name}/{s3_key}")
+
+
+def download_file_s3(
+        local_checkpoint_file, s3_checkpoint_file, bucket_name="jettrain-experiments"
+):
+    # Initialize the S3 client
+    aws_access_key_id, aws_secret_access_key = (
+        os.environ["AWS_KEY"],
+        os.environ["AWS_SECRET"],
+    )
+    s3 = boto3.client(
+        "s3",
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key,
+    )
+
+    # Ensure the local directory exists
+    local_file_directory = os.path.dirname(local_checkpoint_file)
+    if not os.path.exists(local_file_directory):
+        os.makedirs(local_file_directory)
+
+    # Download the file from S3
+    s3.download_file(bucket_name, s3_checkpoint_file, local_checkpoint_file)
+    print(
+        f"Downloaded {s3_checkpoint_file} from S3://{bucket_name}/{s3_checkpoint_file} to {local_checkpoint_file}"
+    )
+
+
+def upload_directory_s3(
+        checkpoint_folder, s3_directory, bucket_name="jettrain-experiments"
+):
+    # Initialize the S3 client
+    aws_access_key_id, aws_secret_access_key = (
+        os.environ["AWS_KEY"],
+        os.environ["AWS_SECRET"],
+    )
+    s3 = boto3.client(
+        "s3",
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key,
+    )
+
+    # Iterate over the files in the local directory
+    for filename in os.listdir(checkpoint_folder):
+        # Check if it's a file (and not a directory)
+        if os.path.isfile(os.path.join(checkpoint_folder, filename)):
+            # Generate the S3 key for the file
+            s3_key = s3_directory + filename
+
+            # Full path to the local file
+            local_file_path = os.path.join(checkpoint_folder, filename)
+
+            # Upload the file to S3
+            s3.upload_file(local_file_path, bucket_name, s3_key)
+            print(f"Uploaded {filename} to S3://{bucket_name}/{s3_key}")
+
+
+def download_s3_folder(bucket_name, s3_folder, local_dir=None):
+    """
+    Download the contents of a folder directory
+    Args:
+        bucket_name: the name of the s3 bucket
+        s3_folder: the folder path in the s3 bucket
+        local_dir: a relative or absolute directory path in the local file system
+    """
+    aws_access_key_id, aws_secret_access_key = (
+        os.environ["AWS_KEY"],
+        os.environ["AWS_SECRET"],
+    )
+    s3_resource = boto3.resource(
+        "s3",
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key,
+    )
+
+    s3_folder = s3_folder[len(bucket_name) + 6:]
+
+    bucket = s3_resource.Bucket(bucket_name)
+
+    local_dir_path = Path(local_dir) if local_dir is not None else Path.cwd()
+
+    for obj in tqdm(bucket.objects.filter(Prefix=s3_folder)):
+        obj_key_path = Path(obj.key)
+        if obj.key.endswith("/"):
+            continue
+
+        target_path = (
+            obj_key_path
+            if local_dir is None
+            else local_dir_path / obj_key_path.relative_to(Path(s3_folder).parent)
+        )
+        target_path.parent.mkdir(parents=True, exist_ok=True)
+        bucket.download_file(obj.key, str(target_path))
+
+    return str(local_dir_path / Path(s3_folder).name)
+
+
+def check_if_file_exists(bucket, key):
+    s3 = boto3.client("s3")
+    try:
+        s3.head_object(Bucket=bucket, Key=key)
+        return True
+    # TODO specify the error
+    except:
+        return False
---
+++ trainer/trainer.py
@@ -0,0 +1,336 @@
+from pathlib import Path
+import shutil
+import random
+import warnings
+from tqdm import tqdm
+import torch
+import torch.nn.functional as tofu
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    get_linear_schedule_with_warmup,
+    get_cosine_schedule_with_warmup,
+    StoppingCriteriaList,
+    StoppingCriteria,
+)
+import wandb
+from math import ceil
+
+from data.s3_data_exchange import upload_directory_s3
+
+
+
+class Trainer:
+    def __init__(
+        self,
+        config,
+        train_data_loader,
+        val_data_loader,
+        perform_sanity_check=True,
+    ):
+        self.train_dataloader = train_data_loader
+        self.val_dataloader = val_data_loader
+        self.sanity_check_complete = False
+
+        self.config = config
+        self.device = self.config.device
+        random_id = random.randint(1, 100000)
+        self.wand_run_name = f"{self.config.model[:5]}_{self.config.parent_dataset}_len-{self.config.sequence_length}_batch-{self.config.accumulate_grad}_lr-{self.config.max_lr}--id{random_id}"
+
+        self.model = AutoModelForCausalLM.from_pretrained(
+            config["model"],
+            torch_dtype=torch.bfloat16,
+            attn_implementation="flash_attention_2",
+        )
+        self.tokenizer = AutoTokenizer.from_pretrained(config["model"])
+
+        self.scheduler_mapping = {
+            "linear": get_linear_schedule_with_warmup,
+            "cosine": get_cosine_schedule_with_warmup,
+        }
+
+        self.model.to(self.device)
+        self.accum_steps = config["accumulate_grad"]
+
+        self.opt = torch.optim.AdamW(
+            self.model.parameters(),
+            lr=self.config["max_lr"],
+            weight_decay=self.config["weight_decay"],
+        )
+
+        self.scheduler = None
+        self.define_scheduler()
+
+        self.checkpoint_steps = []
+        self.val_steps = []
+        self.setup_validation_and_checkpoint_schedule()
+
+        self.batches_done = 0
+        self.mini_batches_done = 0
+        self.loss_acc = 0
+
+        if perform_sanity_check:
+            self.sanity_check()
+
+    def compute_warmup(self):
+        if type(self.config["warmup_steps"]) is int:
+            return self.config["warmup_steps"]
+        elif type(self.config["warmup_steps"]) is float:
+            total_steps = (
+                len(self.train_dataloader) // self.accum_steps
+            ) * self.config["num_epochs"]
+            return ceil(self.config["warmup_steps"] * total_steps)
+        else:
+            raise ValueError(
+                f"Invalid value for `warmup_steps`, got {type(self.config['warmup_steps'])}"
+            )
+
+    def define_scheduler(self):
+        warmup_steps = self._compute_warmup()
+        match self.config["scheduler"]:
+            case "linear":
+                self.scheduler = get_linear_schedule_with_warmup(
+                    self.opt,
+                    warmup_steps,
+                    (len(self.train_dataloader) // self.accum_steps)
+                    * self.config["num_epochs"],
+                )
+            case "cosine":
+                self.scheduler = get_cosine_schedule_with_warmup(
+                    self.opt,
+                    warmup_steps,
+                    (len(self.train_dataloader) // self.accum_steps)
+                    * self.config["num_epochs"],
+                )
+            case _:
+                raise ValueError(
+                    f"Invalid choice of scheduler {self.config['scheduler']}"
+                )
+
+    def run_epoch(self):
+        if not self.sanity_check_complete:
+            self.validation()
+            self.save_hf_checkpoint()
+            self.sanity_check_complete = True
+
+        # TODO replace or add samples
+        wandb.log({"train/total_minibatches": len(self.train_dataloader)})
+
+        for batch_idx, batch in tqdm(
+            enumerate(self.train_dataloader), total=len(self.train_dataloader)
+        ):
+            to_log = self.training_step(batch, batch_idx)
+            wandb.log(to_log)
+
+            if len(self.train_dataloader) < self.accum_steps:
+                warnings.warn(
+                    "No optimizer steps will be done since self.accum_steps > num_of_batches_in_dataloader"
+                )
+
+            if self.mini_batches_done in self.val_steps:
+                print(f"validation on step {self.batches_done}")
+                to_log = self.validation()
+                wandb.log(to_log)
+                self.val_steps.remove(self.mini_batches_done)
+
+            if self.mini_batches_done in self.checkpoint_steps:
+                print(f"checkpoint on step: {self.batches_done}")
+                self.save_hf_checkpoint()
+                self.checkpoint_steps.remove(self.mini_batches_done)
+
+        to_log = self.validation()
+        wandb.log(to_log)
+        self.save_hf_checkpoint()
+
+    def run_training(self):
+        total_epochs = self.config["num_epochs"]
+        for epoch_id in range(total_epochs):
+            print(f"Epoch {epoch_id} / {total_epochs} start")
+            self.run_epoch()
+
+    def validation(self):
+        self.model.eval()
+
+        log_humaneval = self.generate_humaneval_task()
+
+        full_val_loss = 0
+        for batch_idx, batch in enumerate(self.val_dataloader):
+            batch = {k: v.to(self.device) for k, v in batch.items()}
+
+            with torch.no_grad():
+                outputs = self.model_step(batch, return_dict=True)
+                full_val_loss += outputs["loss"]
+
+            if batch_idx == 3:
+                log_batch = log_processed_batch(
+                    outputs["logits"],
+                    outputs["loss_tensor"],
+                    batch["input_ids"],
+                    self.tokenizer,
+                )
+        val_loss_mean = full_val_loss / len(self.val_dataloader)
+
+        # TODO: refactor duplicated code?
+        full_humaneval_loss = 0
+        for batch_idx, batch in enumerate(self.human_eval_loader):
+            batch = {k: v.to(self.device) for k, v in batch.items()}
+
+            with torch.no_grad():
+                outputs = self.model_step(batch, return_dict=True)
+                full_humaneval_loss += outputs["loss"]
+
+        humaneval_loss_mean = full_humaneval_loss / len(self.val_dataloader)
+
+        self.model.train()
+
+        to_log = {
+            "val/val_loss_mean": val_loss_mean,
+            "val/humaneval_loss_mean": humaneval_loss_mean,
+        }
+        to_log.update(log_batch)
+        to_log.update(log_humaneval)
+
+        return to_log
+
+    def model_step(self, batch, return_dict=False):
+        outputs = self.model(**batch, return_dict=True)
+        targets = batch["input_ids"][:, 1:]
+        logits = outputs["logits"][:, :-1]
+        logits = logits.to(torch.float32)
+        loss_mask = batch["attention_mask"][:, 1:]
+        loss_tensor = tofu.cross_entropy(
+            logits.reshape(-1, logits.shape[-1]), targets.reshape(-1), reduction="none"
+        )
+
+        loss_tensor = loss_mask * loss_tensor.reshape_as(loss_mask)
+        loss = loss_tensor.sum() / loss_mask.sum()
+
+        if return_dict:
+            return {"loss": loss, "logits": logits, "loss_tensor": loss_tensor}
+
+        return loss
+
+    def training_step(self, batch, batch_idx):
+        to_log = {}
+
+        batch = {k: v.to(self.device) for k, v in batch.items()}
+
+        loss = self.model_step(batch) / self.accum_steps
+        loss.backward()
+
+        grad_norm = torch.nn.utils.clip_grad_norm_(
+            self.model.parameters(), self.config.max_norm, norm_type=2.0
+        )
+
+        to_log["train/loss_mini_batch"] = loss
+        to_log["grad_norm"] = grad_norm
+
+        self.loss_acc += loss.item()
+        if (batch_idx % self.accum_steps) == (self.accum_steps - 1):
+            self.opt.step()
+            self.opt.zero_grad()
+            lr = next(iter(self.opt.param_groups))["lr"]
+            self.scheduler.step()
+
+            to_log["train/loss_batch"] = self.loss_acc
+            to_log["lr"] = lr
+
+            self.batches_done += 1
+            self.loss_acc = 0.0
+
+        self.mini_batches_done += 1
+        return to_log
+
+    def save_ckpt(self):
+        pth = self.config.get("model_checkpoints")
+        local_path = Path(f"{pth}/{self.wand_run_name}-{self.batches_done}")
+        local_path.mkdir(parents=True, exist_ok=True)
+
+        self.model.save_pretrained(str(local_path))
+        self.tokenizer.save_pretrained(str(local_path))
+
+        if self.config.upload_to_s3:
+            s3_path = self.config.get("s3_checkpoint_path")
+            attempts = 0
+            while attempts < 3:
+                try:
+                    upload_directory_s3(
+                        local_path,
+                        f"{s3_path}/{self.wand_run_name}-{self.batches_done}/",
+                        self.config.get("s3_bucket"),
+                    )
+                    attempts = 3
+
+                except Exception as e:
+                    print("While saving checkpoint:", e)
+                    attempts += 1
+
+            shutil.rmtree(str(local_path))
+
+    def sanity_check(self):
+        print("Running sanity check")
+
+        print("Checking validation")
+        try:
+            self.validation()
+            print("Validation ok")
+        except Exception as e:
+            raise Exception(f"Validation fails with error: {e}")
+
+        print("Checking saving")
+        try:
+            self.save_hf_checkpoint()
+            print("Saving checkpoint ok")
+        except Exception as e:
+            raise Exception(f"Saving checkpoint fails with error: {e}")
+
+        print("Checking training step")
+        try:
+            batch = next(iter(self.train_dataloader))
+            self.training_step(batch, 0)
+            print("Training ok")
+        except Exception as e:
+            raise Exception(f"Train step fails with error: {e}")
+
+        self.sanity_check_complete = True
+
+        print(
+            "Sanity check complete, but remember -- real insanity is doing the same thing"
+            " over and over and expecting different results"
+        )
+
+    def setup_validation_and_checkpoint_schedule(self):
+        self.checkpoint_steps = list(
+            range(
+                1,
+                len(self.train_dataloader) * self.config["num_epochs"],
+                self.config.save_hf_checkpoints_every * self.accum_steps,
+            )
+        )
+        self.val_steps = list(
+            range(
+                1,
+                len(self.train_dataloader) * self.config["num_epochs"],
+                self.config.validate_every * self.accum_steps,
+            )
+        )
+
+        print(
+            f"Validation every: {self.config.validate_every * self.accum_steps}, in total: {len(self.val_steps)} validations"
+        )
+        print(
+            f"Saving checkpoints every: {self.config.save_hf_checkpoints_every * self.accum_steps}, in total: {len(self.checkpoint_steps)} saves"
+        )
+
+# def log_processed_batch(logits, losses, batch, tokenizer):
+#     all_examples = []
+#     for example_id, example_logits in enumerate(logits):
+#         response = process_example(
+#             example_logits, losses[example_id], batch[example_id], tokenizer
+#         )
+#         all_examples.append(response)
+#
+#     html_log = "<br>----------------------<br>".join(all_examples)
+#     to_log = {"Processed_val_batch": wandb.Html("".join(html_log))}
+#
+#     return to_log
